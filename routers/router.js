91f707be4d24c16c600fa0d1ad57a78d2c5ee739
Muamar Al Farabi <muamaralfarabi@gmail.com>
2020-12-16 20:51:54 +0700

    Added Front Page and new css lib

--- routers/router.js
+++ routers/router.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => res.send('LANDING PAGE'));
+router.get('/', (req, res) => res.render('index'));
 
-router.get('/login', (req,res) => res.send('LOGIN PAGE'));
+router.get('/login', (req, res) => res.render('login'));
 
-router.get('/signup', (req,res) => res.send('SIGN UP PAGE'));
+router.get('/signup', (req, res) => res.render('register'));
 
-router.get('/chat', (req,res) => res.send('CHAT LIST PAGE'));
+router.get('/about', (req, res) => res.render('about'));
 
-router.get('/chat/:id', (req,res) => res.send('CHAT ROOM PAGE'));
+router.get('/chat', (req, res) => res.send('CHAT LIST PAGE'));
+
+router.get('/chat/:id', (req, res) => res.send('CHAT ROOM PAGE'));
 
 module.exports = router;
